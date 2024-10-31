aea694eff3835361806c2b751742f0674baa2de9
RamNetSecurity <107959089+RamNetSec@users.noreply.github.com>
2024-10-31 06:10:49 -0600

    1

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect, Suspense } from 'react';
+// ...existing imports...
+
+// Eliminar la importación de ContactSection
+import ProfileSection from './components/ProfileSection';
+import HeroSection from './components/HeroSection';
+// Eliminar la línea: import ContactSection from './components/ContactSection';
+import AcademicHistorySection from './components/AcademicHistorySection';
+// ...rest of the imports...
